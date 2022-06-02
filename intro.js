09b79c423e82188f8ba5e05ea061bd2cc161a868
fareedjaved203 <fareedjaved203@gmail.com>
2022-06-02 22:11:17 +0500

    second commit

--- intro.js
+++
@@ -1,9 +0,0 @@
-// $(window).on('load', function () {
-//     $(".intro").fadeOut(1000);
-//     $(".content").fadeIn(1000);
-// });
-
-
-$(window).on('load', function () {
-    $('.intro').hide();
-}) 
